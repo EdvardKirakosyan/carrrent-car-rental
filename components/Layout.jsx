eb0ccb585c854516536141008d3d3a56c317f4b0
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-04 15:52:54 +0400

    c

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,13 +4,13 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default function Layout() {
-    return (
-        <div className="site-wrapper">
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="site-wrapper">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
 }
--- pages/Host/HostVanPhotos.jsx
+++ pages/Host/HostVanPhotos.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPhotos() {
-    const { currentVan } = useOutletContext()
-    return (
-        <img src={currentVan.imageUrl} className="host-van-detail-image" />
-    )
+  const { currentVan } = useOutletContext()
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />
 }
