cadaf0df621b42315ab9a8771a9c117e4400a43d
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-04 16:08:15 +0400

    c

--- server.js
+++ server.js
@@ -8,68 +8,56 @@ createServer({
   seeds(server) {
     server.create("van", {
       id: "1",
-      name: "Modest Explorer",
+      name: "Chevrolet Camaro (1969)",
       price: 60,
-      description:
-        "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png",
-      type: "simple",
+      description: "description",
+      imageUrl: "/assets/images/Chevrolet Camaro (1969).png",
+      type: "retro",
       hostId: "123",
     })
     server.create("van", {
       id: "2",
-      name: "Beach Bum",
+      name: "Audi RS4 (2022)",
       price: 80,
-      description:
-        "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png",
-      type: "rugged",
+      description: "description",
+      imageUrl: "/assets/images/Audi-RS4-2022-MY.png",
+      type: "sport",
       hostId: "123",
     })
     server.create("van", {
       id: "3",
-      name: "Reliable Red",
+      name: "Rolls Royce Cullinan (2018)",
       price: 100,
-      description:
-        "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png",
-      type: "luxury",
+      description: "description",
+      imageUrl: "/assets/images/Rolls-Royce-Cullinan-2018-INT.png",
+      type: "lux",
       hostId: "456",
     })
     server.create("van", {
       id: "4",
-      name: "Dreamfinder",
+      name: "Aston Martin DB9 GT (2016)",
       price: 65,
-      description:
-        "Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png",
-      type: "simple",
+      description: "description",
+      imageUrl: "/assets/images/Aston Martin DB9 GT (2016) FR.png",
+      type: "lux",
       hostId: "789",
     })
     server.create("van", {
       id: "5",
-      name: "The Cruiser",
+      name: "Porsche 911 Turbo (2020)",
       price: 120,
-      description:
-        "The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png",
-      type: "luxury",
+      description: "description",
+      imageUrl: "/assets/images/Porsche-911-Turbo-2020-INT.png",
+      type: "sport",
       hostId: "789",
     })
     server.create("van", {
       id: "6",
-      name: "Green Wonder",
+      name: "Jaguar E Pace (2017)",
       price: 70,
-      description:
-        "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
-      imageUrl:
-        "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png",
-      type: "rugged",
+      description: "description",
+      imageUrl: "/assets/images/Jaguar-E-Pace-2017-FR.png",
+      type: "lux",
       hostId: "123",
     })
   },
